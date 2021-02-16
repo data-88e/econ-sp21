0b2fb01a5b39ecbb08de34815bc8129a59bb25ae
Chris Pyles <cpyles@berkeley.edu>
2021-02-16 09:50:52 -0800

    lab04

---
+++ lab/lab04/tests/q2_2.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q2_2',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> data.num_rows == 12376\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> len(data.labels) == 52\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(data.column("cgdpe").item(-1), 33028.40234)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
