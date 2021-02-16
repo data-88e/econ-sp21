e76681842f9240e94321be1d2196de4217a8c1bf
Chris Pyles <cpyles@berkeley.edu>
2021-02-16 09:55:44 -0800

    proj02

---
+++ proj/proj02/tests/q3_4.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q3_4',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> len(china_x) == 28\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> len(china_y) == 28\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
