a3a52303b83300b35191ec915f0a0184f90fa257
Chris Pyles <cpyles@berkeley.edu>
2021-03-02 14:56:29 -0800

    lab06

---
+++ lab/lab06/tests/q2_2.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2_2',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> type(x_val) == float\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> type(y_val) == float\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
