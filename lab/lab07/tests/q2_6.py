e783ecf93e1060530c26d52c6a6e1931e81b906b
Chris Pyles <cpyles@berkeley.edu>
2021-03-09 08:17:57 -1000

    lab07, proj03

---
+++ lab/lab07/tests/q2_6.py
@@ -0,0 +1,1 @@
+test = {'name': 'q2_6', 'points': 1, 'suites': [{'cases': [{'code': '>>> 3 <= uber_price <= 45.21\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
