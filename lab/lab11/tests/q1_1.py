e07b7f7e0bcfae965d836156851c46f28e54d794
Chris Pyles <cpyles@berkeley.edu>
2021-04-20 11:27:26 -0700

    lab11, proj04

---
+++ lab/lab11/tests/q1_1.py
@@ -0,0 +1,1 @@
+test = {'name': 'q1_1', 'points': 1, 'suites': [{'cases': [{'code': '>>> len(X_q1_dummies) == 20\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
